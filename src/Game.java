15688da1f275ab12e3e8d036458fb27ccf75a2d8
Arees Somani <asomani@bayviewglen.ca>
2025-05-14 10:39:19 -0400

    change

--- src/Game.java
+++ src/Game.java
@@ -18,8 +18,8 @@ public class Game {
         System.out.println("Welcome to the Text Adventure Game!");
         Room currentRoom = rooms.get(player.getCurrentRoomId());
         System.out.println(currentRoom.getLongDescription());
-
-        while (true) {
+        boolean gameOver = false;
+        while (!gameOver) {
             
             System.out.print("> ");
             String input = scanner.nextLine();
