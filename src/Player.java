1fd76ff3189cff95f9cb407c94f9cd6047acdec9
Eric Pei <spei@bayviewglen.ca>
2025-05-22 11:13:21 -0400

    max health

--- src/Player.java
+++ src/Player.java
@@ -1,22 +1,24 @@
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Player {
+
     private String currentRoomId;
     private List<Item> inventory;
     private int playerHealth;
-    private int playerStrength;
+    final private int maxHealth = 100;
 
     public Player(String startingRoomId) {
         this.currentRoomId = startingRoomId;
         this.inventory = new ArrayList<>();
         this.playerHealth = 100;
-        this.playerStrength = 15;
     }
 
-    public int getPlayerHealth(){
+    public int getPlayerHealth() {
         return playerHealth;
     }
+
     public String getCurrentRoomId() {
         return currentRoomId;
     }
@@ -37,12 +39,7 @@ public class Player {
         return inventory;
     }
 
-    public void updatePlayerHealth(int healthUpdate){
+    public void updatePlayerHealth(int healthUpdate) {
         playerHealth += healthUpdate;
-        System.out.println("Your health is: " + playerHealth);
     }
-    public void updatePlayerStrength(int strengthUpdate){
-        playerStrength += strengthUpdate;
-    }
-    public void obtainKey()
 }
